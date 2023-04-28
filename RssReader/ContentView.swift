84c79a8e8bcaa9729f7ea2e43f5b1f578967a7b6
idofof <idofwww@gmail.com>
2023-04-28 22:09:18 +0800

    - change the listStyle to plainListStyle

--- RssReader/ContentView.swift
+++ RssReader/ContentView.swift
@@ -44,6 +44,7 @@ struct ContentView: View {
                         }
                     }
                 }
+                .listStyle(PlainListStyle())
             }
             .navigationBarTitle("RssReader")
         }
